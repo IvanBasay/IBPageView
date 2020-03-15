9b4bd2c6b5d4cb3fb5cf0c1c236683d9e249e166
IvanBasay <basajivan@gmail.com>
2020-03-15 16:23:56 +0200

    Initial Commit

---
+++ IBPageView/IBPageView.h
@@ -0,0 +1,19 @@
+//
+//  IBPageView.h
+//  IBPageView
+//
+//  Created by Иван Викторович on 15.03.2020.
+//  Copyright © 2020 BosaIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IBPageView.
+FOUNDATION_EXPORT double IBPageViewVersionNumber;
+
+//! Project version string for IBPageView.
+FOUNDATION_EXPORT const unsigned char IBPageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBPageView/PublicHeader.h>
+
+
